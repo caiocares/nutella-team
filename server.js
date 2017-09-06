0827b8f01e2de0854b43537aea04200b6e2abe15
caiocares <caiocares@homerefill.com.br>
2017-09-06 15:51:28 -0300

    start server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const battlefieldStats = require('battlefield-stats-express');
+const app = express();
+ 
+// Get or use your key from https://battlefieldtracker.com/site-api` 
+const bfs = battlefieldStats('8f0224df-8487-486a-9e37-1e38a888ef99');
+ 
+app.use('/api', bfs)
+app.listen(3000);
