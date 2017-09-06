c3eb66f22cf323271a92d5bb61ec4101577b88c9
caiocares <caiocares@homerefill.com.br>
2017-09-06 16:47:17 -0300

    fix: change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // Get or use your key from https://battlefieldtracker.com/site-api` 
 const bfs = battlefieldStats('8f0224df-8487-486a-9e37-1e38a888ef99');
 
-
+var port = process.env.PORT || 8080;        // set our port
  
 app.use('/api', bfs)
-app.listen(3000);
+app.listen(port);
