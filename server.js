f2c59cd9770959128e728ccad00f5c8401f27ad7
caiocares <caiocares@homerefill.com.br>
2017-09-17 21:48:43 -0300

    allow crossdomain

--- server.js
+++ server.js
@@ -146,6 +146,13 @@ router.route('/cron/delete')
     res.json('iuhul');
   });
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', router);
 
 var port = process.env.PORT || 8080;
